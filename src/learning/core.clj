5e1aaef988426cb0053010c2b7a3f90e4e453508
Lauro Caetano <laurocaetano1@gmail.com>
2012-08-23 21:07:48 -0300

    Added Fibonacci

--- src/learning/core.clj
+++ src/learning/core.clj
@@ -1,3 +1,12 @@
 (ns learning.core)
 
 (defn +two [x] (+ 2 x))
+
+(defn fib
+  ([n]
+     (fib [0 1] n))
+  ([x, n]
+     (if (< (count x) n) 
+       (fib (conj x (+ (last x) (nth x (- (count x) 2)))) n)
+       x))
+  )
--- test/learning/test/core.clj
+++ test/learning/test/core.clj
@@ -3,4 +3,7 @@
   (:use [clojure.test]))
 
 (deftest should-sum-two 
-  (is (= 3 (+two 2)) "1 plus 2 should be 3"))
+  (is (= 3 (+two 1)) "1 plus 2 should be 3"))
+
+(deftest fibonacci-test
+	(is (= (fib 3) [0 1 1])))
